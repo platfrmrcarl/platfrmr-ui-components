0b5d0c3a1982d9669370866888c9b94a4b580bb9
carl <carl@platfrmr.com>
2024-06-19 09:51:12 -0400

    added more skeleton components that are part of dasiyui components

---
+++ projects/ui-components/src/lib/timeline/timeline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-timeline',
+  standalone: true,
+  imports: [],
+  templateUrl: './timeline.component.html',
+  styleUrl: './timeline.component.css'
+})
+export class TimelineComponent {
+
+}
