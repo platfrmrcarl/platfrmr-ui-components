a3f4fc55b19f9396501037a396da4122086b51d3
carl <carl@platfrmr.com>
2024-06-26 00:38:36 -0400

    Updating UI library to latest updating pricingtable Inputs

--- projects/ui-components/src/lib/hero/hero.component.ts
+++ projects/ui-components/src/lib/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'uic-hero',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  @Input() bannerText: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() CTA1: string = '';
+  @Input() CTA2: string = '';
+  @Input() CTA1Link: string = '';
+  @Input() CTA2Link: string = '';
 }
