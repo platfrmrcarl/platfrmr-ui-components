7e124d670e07836f064d1c3ec1cfc0b8aa7f739f
carl <carl@platfrmr.com>
2024-06-25 23:50:03 -0400

    Updated navbar with required Inputs to customized navbar

--- projects/ui-components/src/lib/navbar/navbar.component.ts
+++ projects/ui-components/src/lib/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'uic-navbar',
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  @Input() logo: string = '';
+  @Input() title: string = '';
+  @Input() lnk1: string = '';
+  @Input() lnk2: string = '';
+  @Input() lnk3: string = '';
+  @Input() lnk4: string = '';
+  @Input() lnk_txt_1: string = '';
+  @Input() lnk_txt_2: string = '';
+  @Input() lnk_txt_3: string = '';
+  @Input() lnk_txt_4: string = '';
 }
