1f371b828b5ad4cc03c2598617ae32652d2f6949
carl <carl@platfrmr.com>
2024-06-29 15:14:58 -0400

    Updating onboarding wizard

---
+++ projects/ui-components/src/lib/onboarding/onboarding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-onboarding',
+  standalone: true,
+  imports: [],
+  templateUrl: './onboarding.component.html',
+  styleUrl: './onboarding.component.css'
+})
+export class OnboardingComponent {
+
+}
